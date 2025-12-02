287fa8835dc0acf4fad233b0410ef09094fe9f6f
mohitsarodecmaug25-cpu <mohit.sarode.cmaug25@gmail.com>
2025-12-02 20:16:23 +0530

    Delete ykv directory

--- ykv/frontend/src/services/adminService.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-import {ATHLETES_COACHES_URL} from "../constants/APIConstants.js";
-
-
-export function getAthletesCoaches(){
-    return axios.get(ATHLETES_COACHES_URL);
-}
